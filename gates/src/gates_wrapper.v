e4ebd17123dfe4a476ecf3b0fccef6fd53032dbb
Jordan Collocott <jrdn.collocott@gmail.com>
2025-05-18 21:25:00 +0200

    fix(gates_wrapper): output vector length

--- gates/src/gates_wrapper.v
+++ gates/src/gates_wrapper.v
@@ -1,5 +1,5 @@
-module gates_wrapper(input  a, b,
-                     output y);
+module gates_wrapper(input        a, b,
+                     output [3:0] y);
    gates gates_0 (
                   .a(a),
                   .b(b),
