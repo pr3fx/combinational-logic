96ef40db5ca9c00f9e64a95fe59b49f9900732d2
Jordan Collocott <jrdn.collocott@gmail.com>
2025-05-22 21:19:34 +0200

    feat(full_adder): add module logic

--- full_adder/src/full_adder.sv
+++ full_adder/src/full_adder.sv
@@ -1,7 +1,8 @@
 module full_adder (
-                   input logic  a, b,
+                   input logic  a, b, cin,
                    output logic s, cout
                    );
-   
+   assign s = a ^ b ^ cin;
+   assign cout = (a & b) | (a & cin) | (b & cin);
 
 endmodule // full_adder
--- full_adder/src/full_adder_wrapper.v
+++ full_adder/src/full_adder_wrapper.v
@@ -1,10 +1,11 @@
 module full_adder_wrapper (
-                           input  a, b,
+                           input  a, b, cin,
                            output s, cout
                            );
    full_adder full_adder_0(
                            .a(a),
                            .b(b),
+                           .cin(cin),
                            .s(s),
                            .cout(cout)
                            );
